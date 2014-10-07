def2aa60a8a393a5b4975683c1c5950f49985002
Leire <leirehe@gmail.com>
2014-10-07 12:31:25 +0200

    Añadido test multiplication

---
+++ src/test/java/operaciones/MultiplicationTest.java
@@ -0,0 +1,22 @@
+package operaciones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MultiplicationTest {
+
+    private Multiplication multiplicacion;
+    
+    @Before
+    public void inicializar(){
+        multiplicacion = new Multiplication(2,2);
+    }
+    
+    @Test
+    public void testOperar() {
+        assertEquals(4,multiplicacion.operar());
+    }
+
+}
