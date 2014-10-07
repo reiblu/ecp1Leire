ff5913dd7cd552d1ec8715681905bc0f9f77378b
Leire <leirehe@gmail.com>
2014-10-07 10:55:13 +0200

    Creado metodo abstracto

--- src/main/java/operaciones/Operation.java
+++ src/main/java/operaciones/Operation.java
@@ -1,7 +1,25 @@
 package operaciones;
 
 public abstract class Operation {
-    
-    
+
+    private int operador1;
+
+    private int operador2;
+
+    public Operation(int operador1, int operador2) {
+        super();
+        this.operador1 = operador1;
+        this.operador2 = operador2;
+    }
+
+    public int Getoperador1() {
+        return this.operador1;
+    }
+
+    public int Getoperador2() {
+        return this.operador2;
+    }
+
+    public abstract int operar();
 
 }
