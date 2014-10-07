9a78536311a3ee1f5131c7462bc5bebe58cd669d
Leire <leirehe@gmail.com>
2014-10-07 10:59:54 +0200

    Añadido test OperationsHandler

---
+++ src/test/java/operaciones/OperationsHandlerTest.java
@@ -0,0 +1,29 @@
+package operaciones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OperationsHandlerTest {
+
+    @Test
+    public void testOperationsHandler() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddOperation() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReset() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testTotal() {
+        fail("Not yet implemented");
+    }
+
+}
