4fd44a2c808ac8ce761d408d09527ba7b93da4df
Leire <leirehe@gmail.com>
2014-10-07 12:29:26 +0200

    Añadido test metodo summation y corregido metodo operation

--- src/main/java/operaciones/Operation.java
+++ src/main/java/operaciones/Operation.java
@@ -8,26 +8,19 @@ public abstract class Operation {
 
     public Operation(int operador1, int operador2) {
         super();
-        this.setOperador1(operador1);
+        this.operador1 = operador1;
         this.operador2 = operador2;
     }
-
-    public int Getoperador1() {
-        return this.getOperador1();
+    
+    public int getOperador1() {
+        return operador1;
     }
-
-    public int Getoperador2() {
-        return this.operador2;
+    
+    public int getOperador2() {
+        return operador2;
     }
 
     public abstract int operar();
 
-    public int getOperador1() {
-        return operador1;
-    }
-
-    public void setOperador1(int operador1) {
-        this.operador1 = operador1;
-    }
 
 }
---
+++ src/main/java/operaciones/SummationTest.java
@@ -0,0 +1,22 @@
+package operaciones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SummationTest extends OperationTest {
+    
+    private Summation suma;
+    
+    @Before
+    public void inicializar(){
+        suma = new Summation(3,4);
+    }
+
+    @Test
+    public void testOperar() {
+        assertEquals(7, suma.operar());
+    }
+
+}
